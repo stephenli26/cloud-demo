b4dfdf1e79218efbc7fbeb75ab9bab61b54960d7
stephenli26 <stephen.li.dev@hotmail.com>
2021-09-12 20:03:03 +0800

    ribbon init

--- customer1/src/main/java/com/eureka/customer1/config/MySelfRule.java
+++ customer1/src/main/java/com/eureka/customer1/config/MySelfRule.java
@@ -1,8 +1,9 @@
 package com.eureka.customer1.config;
 
-import com.netflix.loadbalancer.RandomRule;
+//import com.netflix.loadbalancer.RandomRule;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 //在这里配置我们自定义的LoadBalancer策略 如果有大佬想自己扩展算法 需要实现ReactorServiceInstanceLoadBalancer接口
 //@LoadBalancerClients(defaultConfiguration = {name = "CLOUD-PAYMENT-SERVICE", configuration = CustomLoadBalancerConfiguration.class})
 //注意这里的name属性 需要和eureka页面中的服务提供者名字一直 此时页面中是大写
-@LoadBalancerClient(name = "provider",configuration = CustomLoadBalancerConfigurationStephen.class)
+//@LoadBalancerClient(name = "provider",configuration = CustomLoadBalancerConfigurationStephen.class)
 public class MySelfRule {
 
     //将这个对象放入ioc容器
